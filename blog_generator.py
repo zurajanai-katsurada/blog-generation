678e7928615790659afd0e1990d6bc56149f2fe9
zurajanai-katsurada <110736389+zurajanai-katsurada@users.noreply.github.com>
2025-12-12 21:35:01 +0600

    Add files via upload

---
+++ blog_generator.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
+client = OpenAI(api_key=config["API_KEY"])
+
+def generate_blog(paragraph_topic):
+    response = client.completions.create(
+        model= 'gpt-3.5-turbo-instruct',
+        prompt=f"Write a paragraph about this topic: {paragraph_topic}",
+        max_tokens= 400,
+        temperature= 0.3
+    )
+
+    retrieve_blog =  response.choices[0].text
+    return retrieve_blog
+
+keep_writing = True
+
+while keep_writing: 
+    answer = input('Write a paragraph? Y for yes and anything else for no')
+
+    if (answer == 'Y'):
+        paragraph_topic = input('What should the paragraph talk about? ')
+        print(generate_blog(paragraph_topic))
+
+    else:
+        keep_writing = False
+
+        
